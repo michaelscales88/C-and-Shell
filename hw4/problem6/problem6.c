4476117b0a6536517002e04da11ffa7d02874f77
Michael Scales <michael.scales88@gmail.com>
2018-04-03 01:43:55 -0500

    hw4

--- hw4/problem6/problem6.c
+++ hw4/problem6/problem6.c
@@ -2,15 +2,44 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
+
 
 int foo_system(const char *command);
 
 int main(int argc, char* argv[]) {
+   if (argc < 2) {
+      fprintf(stderr, "%s takes at least one argument.", argv[0]);
+      exit(1);
+   }
 
-   exit(0);
+   pid_t pid = fork();
+
+   char cmd[500];
+
+   if (pid == -1) {
+    // error, failed to fork()
+   } else if (pid > 0) {
+      wait(NULL);
+   } else {
+      // we are the child
+      strcpy(cmd, argv[1]);
+      for (int i = 2; i < argc - 2; i++) {
+         strcat(cmd, " ");
+         strcat(cmd, argv[i]);
+      }
+      foo_system(cmd);
+      // Failure on execl failure
+      _exit(EXIT_FAILURE);
+   }
+   // Exit from the parent process
+   printf("Exiting\n");
+   exit(EXIT_SUCCESS);
 }
 
 int foo_system(const char *command) {
-
-   return 0;
+   printf("Executing command.\n");
+   execl( "/bin/sh", "sh", "-c", command, (char *) 0);
+   fprintf(stderr, "Return not expected. Must be an execl() error.\n");
+   return 1;
 }
